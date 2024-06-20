95b686dfa329a11f5525565e0f0ba7ed3bd9b65b
Velossoraptor <taliamolsen@gmail.com>
2024-06-20 13:37:47 -0400

    Finished Temple Page Before Audit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,13 @@
 const currentyear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 const today = new Date();
+const hamButton = document.querySelector('#hamburger');
+const navigation = document.querySelector('nav');
 
 currentyear.innerHTML = today.getFullYear();
 lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
