f864bc1a0642c92e71edd72fc8e95572db6f09f5
Velossoraptor <taliamolsen@gmail.com>
2024-07-17 14:30:31 -0400

    Finished Information page

--- project/scripts/home.js
+++ project/scripts/home.js
@@ -10,13 +10,8 @@ let gallery = document.getElementsByClassName("gallery-img");
 let imgs = document.getElementsByClassName("gal-img");
 
 for (let i = 0; i < gallery.length; i++) {
-    // gallery[i].onclick = function (e) { imgZoom(e) }
     gallery[i].addEventListener('click', () => {
         gallery[i].classList.toggle('selectedImg');
         imgs[i].classList.toggle('figSelectedImg');
     })
 }
-
-// function imgZoom(e){
-//     e.classList.toggle('selectedImg');
-// }
